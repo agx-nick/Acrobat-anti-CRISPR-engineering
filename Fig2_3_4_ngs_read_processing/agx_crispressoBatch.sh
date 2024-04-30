97cbe77c54729cadb538c8a64c09c7ba7af258db
agx-nick <nick.hughes@acrobatgenomics.com>
2024-04-30 00:47:18 +0000

    2024Apr29_commit

--- Fig2_3_4_ngs_read_processing/agx_crispressoBatch.sh
+++ Fig2_3_4_ngs_read_processing/agx_crispressoBatch.sh
@@ -8,7 +8,7 @@
 conda activate crispresso2_env
 
 ## First specify the reference and base path
-base_path=/home/ec2-user/ngs_data/MiSeq_PE150_20240412/5_crispresso_batch
+base_path="specify file path"
 ## enter base path so make sure 10x sample list is stored here!
 cd ${base_path}
 
@@ -21,10 +21,4 @@ echo $exp_id
 echo $amp_sht
 
 CRISPRessoBatch --batch_settings $amp_sht -p 4 --ignore_substitutions
-
-## deactivating python virtual environment
-# source deactivate
-
-# What variables does SLURM set?
-# env | grep SLURM | sort -V
 
--- Fig2_3_4_ngs_read_processing/agx_cutmerge_pe_default.sh
+++ Fig2_3_4_ngs_read_processing/agx_cutmerge_pe_default.sh
@@ -8,13 +8,12 @@
 conda activate cutadaptenv
 
 ## First specify the reference and base path
-base_path=/home/ec2-user/ngs_data/MiSeq_PE150_20240412
-trim_path=/home/ec2-user/ngs_data/MiSeq_PE150_20240412/1_cutmg
-## enter base path so make sure 10x sample list is stored here!
+base_path="specify filepath"
+trim_path="specify filepath"
 cd ${base_path}
 mkdir ${trim_path}
 
-SAMPLELIST=/home/ec2-user/ngs_data/MiSeq_PE150_20240412/samples.csv
+SAMPLELIST="specify sample list"
 SEED=$(awk "NR==$SLURM_ARRAY_TASK_ID" $SAMPLELIST)
 sample_id=$(echo "$SEED" | cut -d$',' -f1)
 read1_fq=$(echo "$SEED" | cut -d$',' -f2)
@@ -26,7 +25,7 @@ echo $read2_fq
 cd ${trim_path}
 
 cutadapt \
-	-g ACCACTGCCATGTATCAAAGTACG \
+	-g ACCACTGCCATGTATCAAAGTACG \ #seq adaptors
 	-a CGAGGAGGTTCACTGGGTAGTAAG \
 	-G CTTACTACCCAGTGAACCTCCTCG \
 	-A CGTACTTTGATACATGGCAGTGGT \
@@ -41,10 +40,4 @@ flash \
 	-o ${sample_id} \
 	${sample_id}.R1.trimmed.fastq.gz \
 	${sample_id}.R2.trimmed.fastq.gz
-
-## deactivating python virtual environment
-# source deactivate
-
-# What variables does SLURM set?
-# env | grep SLURM | sort -V
 
